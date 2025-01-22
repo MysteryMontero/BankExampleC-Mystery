8d94fb05b8eb83789eb2c24fb736788baab8c223
Mystery <koby.bradley@icloud.com>
2025-01-21 20:27:17 -0500

    Bank Account
    We are currently on HighInterestSavingsAccount

---
+++ BankLibrary/BankLibrary/HighInterestSavingsAccount.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankLibrary
+{
+    public class HighInterestSavingsAccount : SavingsAccount
+    {
+        public HighInterestSavingsAccount(string accountNumber, string accountName, decimal initialBalance, decimal interestrate) :
+            base(accountNumber, accountName, initialBalance, interestrate)
+        {
+            
+        }
+        public void ApplyBonusInterest(decimal bonusRate)
+        {
+            decimal bonusInterestPayment = this.Balance * bonusRate;
+            this.Deposit(bonusInterestPayment);
+        }
+    }
+}
---
+++ BankLibrary/BankLibrary/SavingsAccount.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankLibrary
+{
+    public class SavingsAccount : Account
+    {
+        public decimal Interestrate { get; private set; }
+        public SavingsAccount(string accountNumber, string accountname, decimal initialBalance, decimal interestRate) :
+            base(accountNumber, accountname, initialBalance)
+        {
+            Interestrate = interestRate;
+        }
+        public void ApplyInterest()
+        {
+            decimal interestPayment = Balance * Interestrate;
+            this.Deposit(interestPayment);
+        }
+    }
+}
